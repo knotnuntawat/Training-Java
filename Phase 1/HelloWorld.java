5b19214d6f2c04749ab2a7d5502ba70cc5635e0c
knotnuntawat <nuntawat.subpiputtana@gmail.com>
2021-06-08 15:23:27 +0700

    new

--- Phase 1/HelloWorld.java
+++ Phase 1/HelloWorld.java
@@ -1,10 +1,15 @@
 
 class HelloWorld{
  public static void main(String[] args) {
-     double num1 = 210;
+    /* double num1 = 210;
      int num2 = (int)num1;
      System.out.println(num1);
      System.out.println(num2);
+     */
+    
+String firstName = "John ";
+String lastName = "Doe";
+System.out.println(firstName.concat(lastName));
  }
 
 
