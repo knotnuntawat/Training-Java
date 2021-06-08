038f4a82276a8130b9d0cdb86ad6dbcd78eef616
knotnuntawat <nuntawat.subpiputtana@gmail.com>
2021-06-08 11:39:52 +0700

    new

---
+++ Phase 2/ArrayBasic.java
@@ -0,0 +1,21 @@
+
+
+public class ArrayBasic {
+    public static void main(String[] args) {
+        //การสร้าง Array
+
+        //แบบกำหนดขนาดเอาไว้
+/*int [] number =new int [4];
+//กำหนดค่าลงไป
+number[0] = 10;
+number[1] = 20;
+number[2] = 30;
+number[3] = 40;
+
+System.out.println(number[3]);*/
+
+        //แบบกำหนดสมาชิก
+int []number={10,20,30,40,50};
+System.out.println("ตำแหน่งที่4 "+number[4]);
+    }
+}
